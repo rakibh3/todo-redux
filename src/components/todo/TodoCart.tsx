ebb59ed24b00f5dce4260c89b2ed01459dc0f85d
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-03-02 23:09:02 +0600

    Setup Initial UI

--- src/components/todo/TodoCart.tsx
+++ src/components/todo/TodoCart.tsx
@@ -4,7 +4,7 @@ const TodoCart = () => {
   return (
     <div className="bg-white rounded-md flex justify-between items-center border">
       <input type="checkbox" name="" id="" />
-      <p className="font-semibold">Todo title</p>
+      <p className="font-semibold">Todo Title</p>
       <p>Time</p>
       <p>Description</p>
       <div className=" space-x-5">
